b718a0de25bbeb80464fbcebc732817f5684fad6
심규빈 <skb0516@naver.com>
2025-03-12 15:33:31 +0900

    Chore: 백엔드 기본 구조 세팅
    - springboot 프로젝트 생성
    S12P21B108-63

---
+++ backend/src/test/java/com/ssafy/ddukdoc/DdukdocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ddukdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdukdocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
