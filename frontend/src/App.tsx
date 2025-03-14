b416d5ff39565f651de80ebcf35bf1cc6df30d04
홍석진 <seokbangguri@kakao.com>
2025-03-14 14:16:51 +0900

    Test: MSW 테스트 코드
    S12P21B108-79

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
 // import { useState } from 'react';
 import { Button } from './stories/Button'
+import { postRegister } from './apis/auth';
+
+const { worker } = await import('./mocks/browser')
+
+if (import.meta.env.VITE_NODE_ENV === "development") {
+  worker.start();
+}
 
 function App() {
+  const test = async () => {
+    const res = await postRegister();
+    console.log(res);
+  };
 
   return (
     <div>
       test
-      <Button label='버튼 테스트' />
+      <Button onClick={test} label='버튼 테스트' />
     </div>
   );
 }
