4561ecc3acfc7c58ffda5878468972970688698c
심규빈 <skb0516@naver.com>
2025-03-14 14:00:05 +0900

    Feat: user엔티티 soft delete 제공
    S12P21B108-65

--- backend/src/main/java/com/ssafy/ddukdoc/domain/user/entity/User.java
+++ backend/src/main/java/com/ssafy/ddukdoc/domain/user/entity/User.java
@@ -4,17 +4,23 @@ import com.ssafy.ddukdoc.domain.user.entity.constants.Provider;
 import com.ssafy.ddukdoc.global.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@SQLDelete(sql = "UPDATE users SET deleted_at = DATE_ADD(NOW(), INTERVAL 9 HOUR) WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
 public class User extends BaseEntity {
     @Column(length = 50, nullable = false)
     private String name;
