4d47bb3a8cc26bdbdcf8984e013ec5cc174ed51a
김소운 <sozzykim@naver.com>
2025-03-20 15:15:05 +0900

    Feat: DocSelectCard 컴포넌트 제작 S12P21B108-80

---
+++ frontend/src/components/atoms/buttons/DocSelectCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ToggleButtonProps from './ToggleButtonProps';
+
+export default function DocSelectCard({
+  onToggleClick,
+  className,
+  children,
+  id,
+  isSelected,
+  icon,
+}: ToggleButtonProps) {
+  // 색상 타입에 따른 배경색 클래스 설정
+
+  return (
+    <button
+      className={`${className} ${isSelected ? 'text-gray-white bg-primary-300' : 'bg-gray-200'} mx-20 w-full cursor-pointer rounded-2xl px-5 py-6 text-center font-medium transition-colors`}
+      onClick={() => onToggleClick(id)}
+    >
+      {children}
+      {icon}
+    </button>
+  );
+}
