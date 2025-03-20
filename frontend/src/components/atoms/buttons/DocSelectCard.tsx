ad08a714da836598aa0f2e091349e6223c430af3
김소운 <sozzykim@naver.com>
2025-03-20 17:50:29 +0900

    Fix: 잘못된 import 삭제

--- frontend/src/components/atoms/buttons/DocSelectCard.tsx
+++ frontend/src/components/atoms/buttons/DocSelectCard.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ToggleButtonProps from './ToggleButtonProps';
 
 export default function DocSelectCard({
--- frontend/src/components/atoms/buttons/ShortButton.tsx
+++ frontend/src/components/atoms/buttons/ShortButton.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ButtonProps from './ButtonProps';
 
 interface LongButtonProps extends ButtonProps {
