9e7b389ed889d81f940c50d56d348e27a55a8634
dkgus4012 <dkgus4012@naver.com>
2025-03-21 23:18:14 +0900

    Feat: 핀코드 생성 - 문서 저장 전 랜덤 6자리 핀코드 생성 S12P21B108-67

--- backend/src/main/java/com/ssafy/ddukdoc/domain/contract/service/ContractService.java
+++ backend/src/main/java/com/ssafy/ddukdoc/domain/contract/service/ContractService.java
@@ -1,14 +1,22 @@
 package com.ssafy.ddukdoc.domain.contract.service;
 
-import com.ssafy.ddukdoc.domain.template.dto.TemplateFieldResponseDto;
+import com.ssafy.ddukdoc.domain.document.dto.request.DocumentSaveRequestDto;
+import com.ssafy.ddukdoc.domain.document.entity.Document;
+import com.ssafy.ddukdoc.domain.document.entity.DocumentFieldValue;
+import com.ssafy.ddukdoc.domain.document.repository.DocumentFieldValueRepository;
+import com.ssafy.ddukdoc.domain.document.repository.DocumentRepository;
+import com.ssafy.ddukdoc.domain.template.dto.response.TemplateFieldResponseDto;
 import com.ssafy.ddukdoc.domain.template.entity.Template;
 import com.ssafy.ddukdoc.domain.template.entity.TemplateField;
 import com.ssafy.ddukdoc.domain.template.repository.TemplateFieldRepository;
 import com.ssafy.ddukdoc.domain.template.repository.TemplateRepository;
+import com.ssafy.ddukdoc.domain.user.entity.User;
+import com.ssafy.ddukdoc.domain.user.repository.UserRepository;
 import com.ssafy.ddukdoc.global.error.code.ErrorCode;
 import com.ssafy.ddukdoc.global.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,7 +25,10 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ContractService {
     private final TemplateRepository templateRepository;
+    private final UserRepository userRepository;
     private final TemplateFieldRepository templateFieldRepository;
+    private final DocumentRepository documentRepository;
+    private final DocumentFieldValueRepository documentFieldValueRepository;
 
     public List<TemplateFieldResponseDto> getTemplateFields(String templateCode){
         Template template = templateRepository.findByCode(templateCode)
@@ -28,5 +39,8 @@ public class ContractService {
                 .map(TemplateFieldResponseDto::of).collect(Collectors.toList());
 
         return fieldResponses;
+    }
+    private int generatePinCode(){
+        return (int)(Math.random() * 900000) + 100000; // 100000 ~ 999999
     }
 }
