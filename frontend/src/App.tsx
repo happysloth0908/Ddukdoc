b177658b825d5a8cf43c5b299a5271e3d40b1d70
홍석진 <seokbangguri@kakao.com>
2025-03-24 16:52:26 +0900

    Feat: router 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-// import { useState } from 'react';
-import { postRegister } from './apis/auth';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import molecules from '@/components/molecules';
+import { DocsWrite } from '@/pages/docsWritePages/DocsWrite'
 
 const { worker } = await import('./mocks/browser');
 
@@ -9,10 +9,23 @@ if (import.meta.env.VITE_NODE_ENV === 'development') {
 }
 
 function App() {
-  return;
-  <BrowserRouter>
-    <div></div>;
-  </BrowserRouter>;
+  return (
+    <div className='w-dvw h-dvh flex justify-center items-center'>
+      <BrowserRouter>
+        <div className='w-dvw h-dvh md:max-w-md md:max-h-[80%] border'>
+          <molecules.Header children='test' />
+          <Routes>
+            {/* 문서 작성입니다. */}
+            <Route path='/docs' element={<DocsWrite />} />
+            {/* 마이페이지 */}
+            {/* <Route path='/mypage' element={} /> */}
+            {/* 위변조 검사 */}
+            {/* <Route path='/forgery' element={} /> */}
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
