941df6ac9829a9bf3e33b9415c829df10491630e
김소운 <sozzykim@naver.com>
2025-03-24 13:14:46 +0900

    Feat: Header 스토리북 제작 S12P21B108-96

--- frontend/src/stories/Header.stories.ts
+++ frontend/src/stories/Header.stories.ts
@@ -1,33 +1,25 @@
+// DocSelectCard.stories.tsx
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import { Header } from '@/components/molecules/Header';
 
-import { Header } from './Header';
-
-const meta = {
-  title: 'Example/Header',
+const meta: Meta<typeof Header> = {
+  title: 'Molecules/Header',
   component: Header,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+
   parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
+    layout: 'fullscreen', // 'centered'에서 변경
   },
-  args: {
-    onLogin: fn(),
-    onLogout: fn(),
-    onCreateAccount: fn(),
-  },
-} satisfies Meta<typeof Header>;
-
-export default meta;
-type Story = StoryObj<typeof meta>;
-
-export const LoggedIn: Story = {
-  args: {
-    user: {
-      name: 'Jane Doe',
-    },
+  tags: ['autodocs'],
+  argTypes: {
+    children: { control: 'text' },
   },
 };
 
-export const LoggedOut: Story = {};
+export default meta;
+type Story = StoryObj<typeof Header>;
+
+export const Default: Story = {
+  args: {
+    children: 'Document',
+  },
+};
