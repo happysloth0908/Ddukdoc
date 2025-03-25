a966f933daff45d37d7ec5471b2fd848e1050a71
김소운 <sozzykim@naver.com>
2025-03-25 12:52:15 +0900

    Feat: 로그인 페이지 제작 S12P21B108-98

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import molecules from '@/components/molecules';
-import { DocsWrite } from '@/pages/docsWritePages/DocsWrite'
+// import { DocsWrite } from '@/pages/docsWritePages/DocsWrite';
+import { LoginPage } from '@/pages/loginPages/Login';
 
 const { worker } = await import('./mocks/browser');
 
@@ -10,13 +11,15 @@ if (import.meta.env.VITE_NODE_ENV === 'development') {
 
 function App() {
   return (
-    <div className='w-dvw h-dvh flex justify-center items-center'>
+    <div className="flex h-dvh w-dvw items-center justify-center">
       <BrowserRouter>
-        <div className='w-dvw h-dvh md:max-w-md md:max-h-[80%] px-8 border'>
-          <molecules.Header children='test' />
+        <div className="h-dvh w-dvw border px-8 md:max-h-[80%] md:max-w-md">
+          <molecules.Header children="test" />
           <Routes>
+            {/* 로그인 페이지 */}
+            <Route path="/login" element={<LoginPage />} />
             {/* 문서 작성입니다. */}
-            <Route path='/docs' element={<DocsWrite />} />
+            {/* <Route path="/docs" element={<DocsWrite />} /> */}
             {/* 마이페이지 */}
             {/* <Route path='/mypage' element={} /> */}
             {/* 위변조 검사 */}
