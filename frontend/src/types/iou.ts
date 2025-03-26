513291373c11e7bd6dc0d1e781c540e4ae1c90e1
홍석진 <seokbangguri@kakao.com>
2025-03-26 17:46:49 +0900

    Feat: 차용증 문서 작성 페이지
    S12P21B108-118

---
+++ frontend/src/types/iou.ts
@@ -0,0 +1,23 @@
+export default interface iouData {
+    loan_purpose: string;
+    loan_date: string; // YYYY-MM-DD
+    principal_amount_text: string;
+    principal_amount_numeric: number;
+    interest_rate: number;
+    repayment_date: string; // YYYY-MM-DD
+    bank_name: string;
+    account_holder: string;
+    account_number: string;
+    interest_payment_date: number;
+    late_interest_rate: number;
+    loss_of_benefit_conditions: string;
+    special_terms?: string; // 선택 항목
+    creditor_name: string;
+    creditor_address: string;
+    creditor_contact: string;
+    creditor_id: string;
+    debtor_name: string;
+    debtor_address: string;
+    debtor_contact: string;
+    debtor_id: string;
+}
