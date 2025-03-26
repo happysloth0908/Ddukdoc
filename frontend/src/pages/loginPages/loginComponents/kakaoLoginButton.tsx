23e0bc140c47b4b560b8a73d91f46021195f2375
김소운 <sozzykim@naver.com>
2025-03-26 14:27:23 +0900

    Feat: 로그인 기능 구현 S12P21B108-59

--- frontend/src/pages/loginPages/loginComponents/kakaoLoginButton.tsx
+++ frontend/src/pages/loginPages/loginComponents/kakaoLoginButton.tsx
@@ -5,8 +5,15 @@ interface KakaoLoginButtonProps {
 }
 
 export const KakaoLoginButton = ({ className }: KakaoLoginButtonProps) => {
+  const VITE_KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
+  const VITE_KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+
+  const sendToKakaoLogin = () => {
+    const kakaoLoginURL = `https://kauth.kakao.com/oauth/authorize?client_id=${VITE_KAKAO_CLIENT_ID}&redirect_uri=${VITE_KAKAO_REDIRECT_URI}&response_type=code`;
+    window.location.href = kakaoLoginURL;
+  };
   return (
-    <button className={className}>
+    <button className={className} onClick={sendToKakaoLogin}>
       <img src={Login.kakao_login} alt="카카오 로그인" className="w-full" />
     </button>
   );
