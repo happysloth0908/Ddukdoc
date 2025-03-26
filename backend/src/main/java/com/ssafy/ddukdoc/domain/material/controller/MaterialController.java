88208cf0fee8301e5742fe89191c8bf8ef2041c3
singwisdom <57509973+singwisdom@users.noreply.github.com>
2025-03-26 15:37:20 +0900

    Feat: 기타 자료 추가 구현
    S12P21B108-103

--- backend/src/main/java/com/ssafy/ddukdoc/domain/material/controller/MaterialController.java
+++ backend/src/main/java/com/ssafy/ddukdoc/domain/material/controller/MaterialController.java
@@ -2,9 +2,12 @@ package com.ssafy.ddukdoc.domain.material.controller;
 
 import com.ssafy.ddukdoc.domain.material.service.MaterialService;
 import com.ssafy.ddukdoc.global.common.response.ApiResponse;
+import com.ssafy.ddukdoc.global.security.auth.UserPrincipal;
+import com.ssafy.ddukdoc.global.util.AuthenticationUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,15 +17,18 @@ import org.springframework.web.multipart.MultipartFile;
 public class MaterialController {
 
     private final MaterialService materialService;
+    private final AuthenticationUtil authenticationUtil;
 
     // 기타 자료 추가
     @PostMapping(value = "/{doc_id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResponse<Void>> uploadMaterial(
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
             @PathVariable("doc_id") Integer documentId,
             @RequestPart("title") String title,
-            @RequestPart("file")MultipartFile file){
+            @RequestPart("file") MultipartFile file){
 
-        materialService.uploadMaterial(documentId, title, file);
-        return ApiResponse.ok(null);
+        Integer userId = authenticationUtil.getCurrentUserId(userPrincipal);
+        materialService.uploadMaterial(userId, documentId, title, file);
+        return ApiResponse.ok();
     }
 }
--- backend/src/main/java/com/ssafy/ddukdoc/domain/material/service/MaterialService.java
+++ backend/src/main/java/com/ssafy/ddukdoc/domain/material/service/MaterialService.java
@@ -1,6 +1,16 @@
 package com.ssafy.ddukdoc.domain.material.service;
 
+import com.ssafy.ddukdoc.domain.document.entity.Document;
+import com.ssafy.ddukdoc.domain.document.repository.DocumentRepository;
+import com.ssafy.ddukdoc.domain.material.entity.DocumentEvidence;
 import com.ssafy.ddukdoc.domain.material.repository.MaterialRepository;
+import com.ssafy.ddukdoc.domain.user.entity.User;
+import com.ssafy.ddukdoc.domain.user.repository.UserRepository;
+import com.ssafy.ddukdoc.global.common.util.FileValidationUtil;
+import com.ssafy.ddukdoc.global.common.util.S3Util;
+import com.ssafy.ddukdoc.global.error.code.ErrorCode;
+import com.ssafy.ddukdoc.global.error.exception.CustomException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,10 +19,39 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class MaterialService {
 
+    private final DocumentRepository documentRepository;
+    private final UserRepository userRepository;
     private final MaterialRepository materialRepository;
+    private final FileValidationUtil fileValidationUtil;
+    private final S3Util s3Util;
 
-    public void uploadMaterial(Integer documentId, String title, MultipartFile file){
+    @Transactional
+    public void uploadMaterial(Integer userId, Integer documentId, String title, MultipartFile file){
 
+        // Document 조회, 엔티티를 id로 조회 했을때 없으면 예외 발생
+        Document document = documentRepository.findById(documentId)
+                .orElseThrow(()-> new CustomException(ErrorCode.DOCUMENT_NOT_FOUND, "documentId", documentId));
+
+        // User 조회
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.INVALID_USER_ID, "userId", userId));
+
+        // 파일 검증
+        fileValidationUtil.isValidFileExtension(file);
+        
+        // S3에 파일 업로드 (암호화)
+        String fileUrl = s3Util.uploadEncryptedFile(file, "material");
+
+        // 엔티티 저장
+        DocumentEvidence documentEvidence = DocumentEvidence.builder()
+                .document(document)
+                .user(user)
+                .title(title)
+                .filePath(fileUrl)
+                .mimeType(file.getContentType())
+                .build();
+
+        materialRepository.save(documentEvidence);
     }
 
 }
