da0cf48c5fb0cc1d1608f8dd06f18cf20bafe6d7
김소운 <sozzykim@naver.com>
2025-03-28 15:01:03 +0900

    Test: 위변조 검사 msw 제작

--- frontend/src/mocks/browser.ts
+++ frontend/src/mocks/browser.ts
@@ -1,7 +1,8 @@
 import { setupWorker } from 'msw/browser';
 import { handlers } from './handlers';
 import { mypageHandlers } from './mypageHandlers';
+import { forgeryHandlers } from './fogery.handlers';
 
-const allHandlers = [...handlers, ...mypageHandlers];
+const allHandlers = [...handlers, ...mypageHandlers, ...forgeryHandlers];
 
 export const worker = setupWorker(...allHandlers);
