b51421566b916a888ee29ba9f5ca476f36a47adc
singwisdom <57509973+singwisdom@users.noreply.github.com>
2025-04-01 10:30:13 +0900

    Refactor: DTO에 Schema 추가
    S12P21B108-99

--- backend/src/main/java/com/ssafy/ddukdoc/domain/document/dto/response/DocumentDownloadResponseDto.java
+++ backend/src/main/java/com/ssafy/ddukdoc/domain/document/dto/response/DocumentDownloadResponseDto.java
@@ -1,13 +1,16 @@
 package com.ssafy.ddukdoc.domain.document.dto.response;
 
 import com.ssafy.ddukdoc.domain.document.entity.Document;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class DocumentDownloadResponseDto {
+    @Schema(example = "차용증 문서")
     private String documentTitle;
+    @Schema(description = "PDF 문서의 바이너리 콘텐츠", type = "string", format = "binary")
     private byte[] documentContent;
 
     public static DocumentDownloadResponseDto of(Document document, byte[] content){
