3da101f5741488f965404cee0484302ea05d741e
김소운 <sozzykim@naver.com>
2025-04-01 17:56:03 +0900

    Feat: 로그인 api 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/ssafy-api': {
+        target: 'https://project.ssafy.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ssafy-api/, ''),
+      },
+    },
+  },
   build: {
     rollupOptions: {
       output: {
@@ -26,7 +35,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+});
