48d13dabba549f5a4c535382b405a783ef78e49b
dkgus4012 <dkgus4012@naver.com>
2025-04-02 15:28:30 +0900

    Feat: 싸피 문서 서명 있는 버전 저장 API 구현
    - 서명 X, 서명O/추가문서X, 서명O/추가문서O 인 경우를 나눠서 작성
    
    S12P21B108-196

--- backend/src/main/java/com/ssafy/ddukdoc/domain/contract/controller/SsafyContractController.java
+++ backend/src/main/java/com/ssafy/ddukdoc/domain/contract/controller/SsafyContractController.java
@@ -1,17 +1,24 @@
 package com.ssafy.ddukdoc.domain.contract.controller;
 
 import com.ssafy.ddukdoc.domain.contract.service.SsafyContractService;
+import com.ssafy.ddukdoc.domain.document.dto.request.DocumentSaveRequestDto;
 import com.ssafy.ddukdoc.domain.template.dto.response.TemplateFieldResponseDto;
 import com.ssafy.ddukdoc.global.aop.swagger.ApiErrorCodeExamples;
 import com.ssafy.ddukdoc.global.common.response.CommonResponse;
 import com.ssafy.ddukdoc.global.error.code.ErrorCode;
+import com.ssafy.ddukdoc.global.security.auth.UserPrincipal;
 import com.ssafy.ddukdoc.global.util.AuthenticationUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -29,6 +36,28 @@ public class SsafyContractController {
     @ApiErrorCodeExamples({ErrorCode.TEMPLATE_NOT_FOUND})
     public ResponseEntity<CommonResponse<List<TemplateFieldResponseDto>>> getFieldList(@PathVariable String templateCode){
         return CommonResponse.ok(ssafyContractService.getTemplateFields(templateCode));
+    }
+
+    @PostMapping(value = "/{templateCode}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PreAuthorize("isAuthenticated()")
+    @Operation(summary = "문서 저장", description = "템플릿 코드에 해당하는 문서를 저장합니다. \n\n문서 ID를 return 합니다.")
+    @ApiErrorCodeExamples({ErrorCode.SIGNATURE_FILE_NOT_FOUND, ErrorCode.INVALID_USER_ID, ErrorCode.TEMPLATE_NOT_FOUND, ErrorCode.TEMPLATE_FIELD_NOT_FOUND, ErrorCode.INVALID_USER_ID, ErrorCode.FORBIDDEN_ACCESS, ErrorCode.FILE_UPLOAD_ERROR, ErrorCode.INVALID_INPUT_VALUE})
+    public ResponseEntity<CommonResponse<Integer>> saveInfo(
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @PathVariable String templateCode,
+            @RequestPart("jsonData") @Valid DocumentSaveRequestDto requestDto,
+            @RequestPart(value = "signature", required = false) MultipartFile signatureFile,
+            @RequestPart(value = "proofDocuments", required = false) List<MultipartFile> proofDocuments){
+
+        Integer userId = authenticationUtil.getCurrentUserId(userPrincipal);
+
+        return CommonResponse.ok(ssafyContractService.saveDoc(templateCode, requestDto, userId,signatureFile,proofDocuments));
+    }
+    // 증빙서류 형식 검증 헬퍼 메서드 (이미지 + PDF)
+    private boolean isValidFileType(String contentType) {
+        return contentType.equals("image/png") ||
+                contentType.equals("image/jpeg") ||
+                contentType.equals("image/jpg");
     }
 
 }
--- backend/src/main/java/com/ssafy/ddukdoc/domain/contract/service/SsafyContractService.java
+++ backend/src/main/java/com/ssafy/ddukdoc/domain/contract/service/SsafyContractService.java
@@ -1,19 +1,41 @@
 package com.ssafy.ddukdoc.domain.contract.service;
 
+import com.ssafy.ddukdoc.domain.contract.entity.Signature;
+import com.ssafy.ddukdoc.domain.contract.repository.SignatureRepository;
+import com.ssafy.ddukdoc.domain.document.dto.request.DocumentSaveRequestDto;
+import com.ssafy.ddukdoc.domain.document.entity.Document;
+import com.ssafy.ddukdoc.domain.document.entity.DocumentFieldValue;
+import com.ssafy.ddukdoc.domain.document.entity.DocumentStatus;
+import com.ssafy.ddukdoc.domain.document.repository.DocumentFieldValueRepository;
+import com.ssafy.ddukdoc.domain.document.repository.DocumentRepository;
 import com.ssafy.ddukdoc.domain.template.dto.response.TemplateFieldResponseDto;
+import com.ssafy.ddukdoc.domain.template.entity.Role;
 import com.ssafy.ddukdoc.domain.template.entity.Template;
 import com.ssafy.ddukdoc.domain.template.entity.TemplateCode;
 import com.ssafy.ddukdoc.domain.template.entity.TemplateField;
+import com.ssafy.ddukdoc.domain.template.repository.RoleRepository;
 import com.ssafy.ddukdoc.domain.template.repository.TemplateFieldRepository;
 import com.ssafy.ddukdoc.domain.template.repository.TemplateRepository;
+import com.ssafy.ddukdoc.domain.user.entity.User;
+import com.ssafy.ddukdoc.domain.user.entity.UserDocRole;
+import com.ssafy.ddukdoc.domain.user.repository.UserDocRoleRepository;
+import com.ssafy.ddukdoc.domain.user.repository.UserRepository;
+import com.ssafy.ddukdoc.global.common.util.AESUtil;
+import com.ssafy.ddukdoc.global.common.util.MultipartFileUtils;
+import com.ssafy.ddukdoc.global.common.util.S3Util;
+import com.ssafy.ddukdoc.global.common.util.pdfgenerator.PdfGeneratorUtil;
 import com.ssafy.ddukdoc.global.error.code.ErrorCode;
 import com.ssafy.ddukdoc.global.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,7 +44,16 @@ import java.util.stream.Collectors;
 @Transactional(readOnly = true)
 public class SsafyContractService {
     private final TemplateRepository templateRepository;
+    private final UserRepository userRepository;
     private final TemplateFieldRepository templateFieldRepository;
+    private final DocumentRepository documentRepository;
+    private final DocumentFieldValueRepository documentFieldValueRepository;
+    private final S3Util s3Util;
+    private final SignatureRepository signatureRepository;
+    private final RoleRepository roleRepository;
+    private final UserDocRoleRepository userDocRoleRepository;
+    private final AESUtil aesUtil;
+    private final PdfGeneratorUtil pdfGeneratorUtil;
 
     public List<TemplateFieldResponseDto> getTemplateFields(String  codeStr){
 
@@ -37,6 +68,155 @@ public class SsafyContractService {
 
         return fieldResponses;
     }
+    @Transactional public Integer saveDoc(String codeStr, DocumentSaveRequestDto requestDto, Integer userId, MultipartFile signatureFile, List<MultipartFile> proofDocuments){
+        TemplateCode templateCode = TemplateCode.fromString(codeStr);
 
+        // 서명 유효성 검사
+        if (signatureFile == null || signatureFile.isEmpty()) {
+            throw new CustomException(ErrorCode.SIGNATURE_FILE_NOT_FOUND);
+        }
+
+        // 사용자 조회
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.INVALID_USER_ID));
+
+        // 템플릿 조회
+        Template template = templateRepository.findByCode(templateCode.name())
+                .orElseThrow(() -> new CustomException(ErrorCode.TEMPLATE_NOT_FOUND, "templateCode", templateCode.name()));
+
+        //Document 엔티티 생성 및 저장
+        Document document = requestDto.toEntity(user,template,templateCode);
+        Document saveDocument = documentRepository.save(document);
+
+        // DocumentFieldValue 엔티티들 생성 및 저장
+        saveSenderInfo(requestDto, saveDocument, user);
+
+        switch (templateCode){
+            case S5:  //서명없이 저장
+                return saveDocument.getId();
+            case S2: case S3:  //서명 + 증빙서류 필요
+                return saveDocumentWithSignatureAndExtra(document,saveDocument,user, requestDto, templateCode,signatureFile,proofDocuments);
+            default:  // 서명만 필요
+                return saveDocumentWithSignature(document,saveDocument,user, requestDto, templateCode,signatureFile);
+        }
+    }
+
+    private Integer saveDocumentWithSignatureAndExtra(Document document, Document saveDocument, User user, DocumentSaveRequestDto requestDto, TemplateCode templateCode,MultipartFile signatureFile, List<MultipartFile> proofDocuments) {
+        return 1;
+    }
+
+    private Integer saveDocumentWithSignature(Document document, Document saveDocument, User user, DocumentSaveRequestDto requestDto, TemplateCode templateCode,MultipartFile signatureFile) {
+        try{
+            //서명 파일 저장
+            saveSignature(document,user.getId(),signatureFile);
+
+            //user 역할 저장
+            saveUserDocRole(saveDocument,user,requestDto.getRoleId());
+
+            // 파일 생성 시 필요한 서명 맵 생성
+            Map<Integer,byte[]> signature = new HashMap<>();
+            signature.put(requestDto.getRoleId(),signatureFile.getBytes());;
+
+            // 문서 pdf 생성 및 해시 저장
+            byte[] pdfWithHash = pdfGeneratorUtil.generatePdfForHash(
+                    templateCode,
+                    requestDto.getData(),
+                    signature
+            );
+
+            // 암호화된 PDF 저장
+            String pdfPath = saveEncryptedPdf(pdfWithHash, document);
+
+            //문서 상태 변경
+            updateDocumentStatus(document, pdfPath);
+            return saveDocument.getId();
+
+        } catch (IOException e) {
+            throw new CustomException(ErrorCode.FILE_CONVERT_ERROR,"reason","서명 변환 시 오류 발생");
+        }
+    }
+
+
+    private void saveUserDocRole(Document document, User user, int roleId) {
+        Role role = roleRepository.findById(roleId)
+                .orElseThrow(()-> new CustomException(ErrorCode.INVALID_INPUT_VALUE,"role_id",roleId));
+
+        UserDocRole userDocRole = UserDocRole.builder()
+                .user(user)
+                .document(document)
+                .role(role)
+                .build();
+
+        userDocRoleRepository.save(userDocRole);
+    }
+    //서명 파일을 암호화하여 S3에 저장
+    private void saveSignature(Document document, Integer userId, MultipartFile signatureFile) {
+        // 사용자 조회
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.INVALID_USER_ID, "userId", userId));
+
+        // 사용자 권한 확인
+        if (!document.getCreator().getId().equals(userId) &&
+                (document.getRecipient() == null || !document.getRecipient().getId().equals(userId))) {
+            throw new CustomException(ErrorCode.FORBIDDEN_ACCESS, "userId", userId);
+        }
+
+        String dirName = "signature/"+document.getId()+"/"+userId;
+        String s3Path = s3Util.uploadEncryptedFile(signatureFile,dirName);
+
+
+        // dto로 받는게 아니라서, 바로 builder 패턴 적용
+        Signature signature = Signature.builder()
+                .user(user)
+                .document(document)
+                .filePath(s3Path)
+                .build();
+
+        signatureRepository.save(signature);
+        //수신자가 서명을 한 상태인 경우 문서 상태 SIGNED로 변경
+        if(document.getRecipient() != null && document.getRecipient().getId().equals(userId)){
+            document.updateStatus(DocumentStatus.SIGNED);
+            documentRepository.save(document);
+        }
+    }
+    private void saveSenderInfo(DocumentSaveRequestDto requestDto, Document document, User user) {
+        List<DocumentFieldValue> fieldValues = requestDto.getData().stream()
+                .map(fieldValueDto -> {
+                    TemplateField field = templateFieldRepository.findById(fieldValueDto.getFieldId())
+                            .orElseThrow(() -> new CustomException(ErrorCode.TEMPLATE_FIELD_NOT_FOUND,
+                                    "template_field_id", fieldValueDto.getFieldId()));
+
+                    // 필드 값을 암호화하여 저장
+                    String encryptedValue = aesUtil.encrypt(fieldValueDto.getFieldValue());
+
+                    return fieldValueDto.toEntity(document, field, user,encryptedValue);
+
+                })
+                .collect(Collectors.toList());
+
+        documentFieldValueRepository.saveAll(fieldValues);
+    }
+
+    //암호화된 pdf s3에 저장
+    private String saveEncryptedPdf(byte[] pdfData, Document document) {
+
+        MultipartFile multipartFile = MultipartFileUtils.createMultipartFile(
+                "document.pdf",
+                "document.pdf",
+                "application/pdf",
+                pdfData
+        );
+
+        // PDF 파일을 암호화하여 S3에 업로드
+        String pdfPath = s3Util.uploadEncryptedFile(multipartFile, "contract/" + document.getId());
+
+        return pdfPath;
+    }
+
+    // 문서 상태 변경 메서드
+    private void updateDocumentStatus(Document document, String pdfPath) {
+        document.updateFilePath(pdfPath);
+        documentRepository.save(document);
+    }
 
 }
