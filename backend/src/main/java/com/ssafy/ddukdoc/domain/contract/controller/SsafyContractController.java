223cac9513da59a69b8c1913f2101248ec507cd7
dkgus4012 <dkgus4012@naver.com>
2025-04-02 17:25:43 +0900

    Chore: 싸피 Controller Swagger 변경

--- backend/src/main/java/com/ssafy/ddukdoc/domain/contract/controller/SsafyContractController.java
+++ backend/src/main/java/com/ssafy/ddukdoc/domain/contract/controller/SsafyContractController.java
@@ -26,7 +26,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/ssafy/contract")
 @RequiredArgsConstructor
-@Tag(name = "계약 관리", description = "계약/템플릿 관련 API")
+@Tag(name = "싸피 계약 관리", description = "싸피 계약/템플릿 관련 API")
 public class SsafyContractController {
     private final SsafyContractService ssafyContractService;
     private final AuthenticationUtil authenticationUtil;
@@ -40,7 +40,7 @@ public class SsafyContractController {
 
     @PostMapping(value = "/{templateCode}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @PreAuthorize("isAuthenticated()")
-    @Operation(summary = "문서 저장", description = "템플릿 코드에 해당하는 문서를 저장합니다. \n\n문서 ID를 return 합니다.")
+    @Operation(summary = "싸피 문서 저장", description = "템플릿 코드에 해당하는 문서를 저장합니다. \n\n문서 ID를 return 합니다.")
     @ApiErrorCodeExamples({ErrorCode.SIGNATURE_FILE_NOT_FOUND, ErrorCode.INVALID_USER_ID, ErrorCode.TEMPLATE_NOT_FOUND, ErrorCode.TEMPLATE_FIELD_NOT_FOUND, ErrorCode.INVALID_USER_ID, ErrorCode.FORBIDDEN_ACCESS, ErrorCode.FILE_UPLOAD_ERROR, ErrorCode.INVALID_INPUT_VALUE})
     public ResponseEntity<CommonResponse<Integer>> saveInfo(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
@@ -53,11 +53,4 @@ public class SsafyContractController {
 
         return CommonResponse.ok(ssafyContractService.saveDoc(templateCode, requestDto, userId,signatureFile,proofDocuments));
     }
-    // 증빙서류 형식 검증 헬퍼 메서드 (이미지 + PDF)
-    private boolean isValidFileType(String contentType) {
-        return contentType.equals("image/png") ||
-                contentType.equals("image/jpeg") ||
-                contentType.equals("image/jpg");
-    }
-
 }
