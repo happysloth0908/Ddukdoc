1dce105bf8fbb18d22945b6d9959de7c60e913ec
심규빈 <skb0516@naver.com>
2025-04-03 11:06:19 +0900

    Refactor: private 접근자 설정
    S12P21B108-198

--- backend/src/main/java/com/ssafy/ddukdoc/domain/share/dto/response/MMTeamResponse.java
+++ backend/src/main/java/com/ssafy/ddukdoc/domain/share/dto/response/MMTeamResponse.java
@@ -7,39 +7,15 @@ import lombok.Getter;
 
 import java.util.List;
 
-/**
- * [
- *     {
- *         "id": "7rky4c6zntbp5kxtjn4aeeo6ne",
- *         "create_at": 1735268568537,
- *         "update_at": 1735514056620,
- *         "delete_at": 0,
- *         "display_name": "12기 공통 대전1반",
- *         "name": "s12p10b1",
- *         "description": "",
- *         "email": "",
- *         "type": "O",
- *         "company_name": "",
- *         "allowed_domains": "",
- *         "invite_id": "",
- *         "allow_open_invite": false,
- *         "last_team_icon_update": 1735514056620,
- *         "scheme_id": null,
- *         "group_constrained": null,
- *         "policy_id": null,
- *         "cloud_limits_archived": false
- *     },
- */
-
 @Getter
 @Builder
 public class MMTeamResponse {
 
     @Schema(example = "ismhz6qc5bbfmrtdqwnacudx5o")
-    String userId;
+    private String userId;
     @Schema(example = "m8prdnfy67yz5ex57pw.....")
-    String token;
-    List<MMTeam> teams;
+    private String token;
+    private List<MMTeam> teams;
 
     public static MMTeamResponse of(String userId, String token, List<MMTeam> teams) {
         return MMTeamResponse.builder()
@@ -53,9 +29,9 @@ public class MMTeamResponse {
     @Builder
     public static class MMTeam {
         @Schema(example = "7rky4c6zntbp5kxtjn4aeeo6ne")
-        String id;
+        private String id;
         @Schema(example = "12기 공통 대전1반")
-        String displayName;
+        private String displayName;
 
         public static MMTeam of(String id, String display_name) {
             return MMTeam.builder()
