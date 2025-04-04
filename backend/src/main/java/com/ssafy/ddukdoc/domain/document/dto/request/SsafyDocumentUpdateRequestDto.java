ed96fb0fae3aa6bf838c69b098ed0dc33fbaa71c
singwisdom <57509973+singwisdom@users.noreply.github.com>
2025-04-04 10:58:23 +0900

    Chore: Schmea 문서제목 예시 수정
    S12P21B108-194

--- backend/src/main/java/com/ssafy/ddukdoc/domain/document/dto/request/SsafyDocumentUpdateRequestDto.java
+++ backend/src/main/java/com/ssafy/ddukdoc/domain/document/dto/request/SsafyDocumentUpdateRequestDto.java
@@ -14,7 +14,7 @@ public class SsafyDocumentUpdateRequestDto {
 
     @NotNull(message = "문서 제목은 필수입니다.")
     @Size(min = 1, max = 100, message = "문서 제목은 1자 이상 100자 이하로 입력해주세요.")
-    @Schema(example = "홍길동 차용증")
+    @Schema(example = "김싸피 노트북반출확인서")
     private String title;
 
     @NotNull(message = "문서 입력 data는 필수입니다.")
