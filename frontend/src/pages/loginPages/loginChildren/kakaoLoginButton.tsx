aad9bd7dba17606e0fd9a4b1cbe65035ce12b8a8
홍석진 <seokbangguri@kakao.com>
2025-04-04 16:06:42 +0900

    Fix: 카카오 로그인 redirect uri 수정

--- frontend/src/pages/loginPages/loginChildren/kakaoLoginButton.tsx
+++ frontend/src/pages/loginPages/loginChildren/kakaoLoginButton.tsx
@@ -7,10 +7,9 @@ interface KakaoLoginButtonProps {
 export const KakaoLoginButton = ({ className }: KakaoLoginButtonProps) => {
   const VITE_KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
   const VITE_KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
-  const VITE_API_URL = import.meta.env.VITE_API_URL;
 
   const sendToKakaoLogin = () => {
-    const kakaoLoginURL = `https://kauth.kakao.com/oauth/authorize?client_id=${VITE_KAKAO_CLIENT_ID}&redirect_uri=${VITE_API_URL + VITE_KAKAO_REDIRECT_URI}&response_type=code`;
+    const kakaoLoginURL = `https://kauth.kakao.com/oauth/authorize?client_id=${VITE_KAKAO_CLIENT_ID}&redirect_uri=${VITE_KAKAO_REDIRECT_URI}&response_type=code`;
     window.location.href = kakaoLoginURL;
   };
   return (
