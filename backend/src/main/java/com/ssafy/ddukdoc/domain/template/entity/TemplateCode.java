06ac5e8103530fb3ab27668ee2636c34bb696f03
HANoeJ <63058756+HANoeJ@users.noreply.github.com>
2025-04-05 18:45:43 +0900

    Refactor: 필요없는 변경 사항 수정

--- backend/src/main/java/com/ssafy/ddukdoc/domain/template/entity/TemplateCode.java
+++ backend/src/main/java/com/ssafy/ddukdoc/domain/template/entity/TemplateCode.java
@@ -4,20 +4,19 @@ import com.ssafy.ddukdoc.global.error.code.ErrorCode;
 import com.ssafy.ddukdoc.global.error.exception.CustomException;
 
 public enum TemplateCode {
-    G1("차용증",1),
-    G2("근로계약서",2),
-    S1("노트북 반출 서약서",3),
-    S2("노트북 수령 확인서",4),
-    S3("출결 확인서",5),
-    S4("출결 변경요청서",6),
-    S5("소스코드 반출 요청서",7),
-    S6("프로젝트 활용 동의서",8);
+    G1("차용증"),
+    G2("근로계약서"),
+    S1("노트북 반출 서약서"),
+    S2("노트북 수령 확인서"),
+    S3("출결 확인서"),
+    S4("출결 변경요청서"),
+    S5("소스코드 반출 요청서"),
+    S6("프로젝트 활용 동의서");
 
     private final String description;
-    private final Integer code;
 
-    TemplateCode(String description, Integer code) {
-        this.description = description; this.code = code;
+    TemplateCode(String description) {
+        this.description = description;
     }
 
     public static TemplateCode fromString(String code){
