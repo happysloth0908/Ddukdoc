27cc4c2e924b9ec0bcfbad2f8a3251566f696a6b
심규빈 <skb0516@naver.com>
2025-04-06 17:05:23 +0900

    Fix: 잘못된 validation annotation
    - NotBlank -> NotNull
    S12P21B108-242

--- backend/src/main/java/com/ssafy/ddukdoc/domain/share/dto/request/MMMessageToChannelRequest.java
+++ backend/src/main/java/com/ssafy/ddukdoc/domain/share/dto/request/MMMessageToChannelRequest.java
@@ -2,6 +2,7 @@ package com.ssafy.ddukdoc.domain.share.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -18,7 +19,7 @@ public class MMMessageToChannelRequest {
     @NotBlank(message = "channelId는 필수입니다.")
     private String channelId;
     @Schema(example = "12")
-    @NotBlank(message = "documentId는 필수입니다.")
+    @NotNull(message = "documentId는 필수입니다.")
     private Integer documentId;
     @Schema(example = "안녕하세요")
     @NotBlank(message = "message는 필수입니다.")
