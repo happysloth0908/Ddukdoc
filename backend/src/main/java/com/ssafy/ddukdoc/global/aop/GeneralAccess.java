68962ee2692af4b48b05195adc75566032be9c68
심규빈 <skb0516@naver.com>
2025-04-07 17:57:35 +0900

    Feat: 회원구분 어노테이션 생성
    S12P21B108-256

---
+++ backend/src/main/java/com/ssafy/ddukdoc/global/aop/GeneralAccess.java
@@ -0,0 +1,13 @@
+package com.ssafy.ddukdoc.global.aop;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAnyRole('GENERAL', 'ADMIN')")
+public @interface GeneralAccess {}
