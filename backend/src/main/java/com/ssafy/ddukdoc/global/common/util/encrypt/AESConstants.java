80ddd60080f4beaaebe74bb3833b124f65b89834
심규빈 <skb0516@naver.com>
2025-04-07 14:39:27 +0900

    Feat: 암호화 상수 클래스
    S12P21B108-244

---
+++ backend/src/main/java/com/ssafy/ddukdoc/global/common/util/encrypt/AESConstants.java
@@ -0,0 +1,6 @@
+package com.ssafy.ddukdoc.global.common.util.encrypt;
+
+public class AESConstants {
+    public static final int GCM_TAG_LENGTH = 128;
+    public static final int IV_LENGTH = 12;
+}
