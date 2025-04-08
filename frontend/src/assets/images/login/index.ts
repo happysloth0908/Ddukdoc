bfe83db991ddc449bbc899293f04bdb65e44a009
김소운 <sozzykim@naver.com>
2025-04-08 21:42:20 +0900

    Design: 로그인 화면 디자인 수정 S12P21B108-289

--- frontend/src/assets/images/login/index.ts
+++ frontend/src/assets/images/login/index.ts
@@ -1,11 +1,13 @@
 import kakao_login from './kakao_login.svg';
 import logo from './logo.svg';
 import title from './title.svg';
+import hammer from './hammer.svg'
 
 const Login = {
   kakao_login,
   logo,
   title,
+  hammer,
 };
 
 export default Login;
